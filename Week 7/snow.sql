118f55e49160f42cf7e9d2ab9c5d541537628b46
agatamilia <amiliaagata@student.telkomuniversity.ac.id>
2023-11-15 19:19:07 +0700

    Week 7

---
+++ Week 7/snow.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 15, 2023 at 01:16 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `snow`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `snowdiarys`
+--
+
+CREATE TABLE `snowdiarys` (
+  `idBook` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `snowdiarys`
+--
+
+INSERT INTO `snowdiarys` (`idBook`, `title`, `stock`, `price`) VALUES
+(999, 'aku', 1, 12000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
